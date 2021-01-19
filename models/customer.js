8a6599ac3279b9b64d0c128524eec99a7c7ada50
Akinyemi Olalekan <69955888+primus02@users.noreply.github.com>
2021-01-19 12:32:18 +0000

    Update customer.js

--- models/customer.js
+++ models/customer.js
@@ -3,6 +3,8 @@ const mongoose= require("mongoose");
 const customerSchema = mongoose.Schema({
     name: {type: String, required: true},
     username: {type: String, required: true},
+    mobile: {type: String, required: true},
+    address: {type: String, required: true},
     email: {type: String, required: true, unique: true, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
     password: {type: String, required: true}
 });
